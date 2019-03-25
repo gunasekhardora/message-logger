aaaad029539a784b458449c05fa5f6c4c32bb40b
gunasekhardora <gunasekhar.dora3@gmail.com>
2019-03-25 23:22:16 +0530

    feat:Add CORS support and POST end point

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';  
 import { MatInputModule,
     MatCardModule, 
     MatButtonModule, 
     MatToolbarModule,
     MatExpansionModule
-  } from '@angular/material';
-import { HttpClientModule } from '@angular/common/http';  
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
