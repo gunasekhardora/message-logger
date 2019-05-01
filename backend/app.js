a70370d5f299884a8f8a6dc24981ab1eae40b7f7
gunasekhardora <gunasekhar.dora3@gmail.com>
2019-05-01 16:51:19 +0530

    feat: add delete post api

--- backend/app.js
+++ backend/app.js
@@ -36,10 +36,13 @@ app.post("/api/posts", (req, res, next) => {
         title: req.body.title,
         content: req.body.content
     });
-    post.save();
-    res.status(201).json({
-        message: 'Post added successfully'
+    post.save().then(result => {
+        res.status(201).json({
+            message: 'Post added successfully',
+            postId : result._id
+        });
     });
+    
 });
 
 app.get('/api/posts', (req, res, next) => {
@@ -63,6 +66,15 @@ app.get('/api/posts', (req, res, next) => {
             });
         });
     
+});
+
+app.delete('/api/posts/:id', (req, res, next) => {
+    Post.deleteOne({_id : req.params.id}).then(result => {
+        console.log(result);
+        res.status(200).json({
+            message : 'Post deleted!'
+        });
+    });
 });
 
 module.exports = app;
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 
 import { Post } from './post.model';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
@@ -12,10 +13,19 @@ export class PostsService {
     constructor(private http: HttpClient) {}
 
     getPosts() {
-        this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
+        this.http.get<{message: string, posts: any }>('http://localhost:3000/api/posts')
+        .pipe(map( (postData) => {
+            return postData.posts.map( post => {
+                return {
+                    title : post.title,
+                    content : post.content,
+                    id : post._id
+                }
+            });
+        }))
         .subscribe(
-            (postData) => {
-                this.posts = postData.posts;
+            (transformedPosts) => {
+                this.posts = transformedPosts;
                 this.postsUpdated.next([...this.posts]);
             }
         );
@@ -27,11 +37,21 @@ export class PostsService {
 
     addPost(title: string, content: string) {
         const post: Post = {id: null, title: title, content: content};
-        this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+        this.http.post<{message: string, postId: string}>('http://localhost:3000/api/posts', post)
             .subscribe( (responseData) => {
-                console.log(responseData.message);
+                const postId = responseData.postId;
+                post.id = postId;
                 this.posts.push(post);
                 this.postsUpdated.next([...this.posts]);
             });
+    }
+
+    deletePost(postId: string) {
+        this.http.delete("http://localhost:3000/api/posts/" + postId)
+        .subscribe(() => {
+            const updatedPosts = this.posts.filter(post => post.id !== postId);
+            this.posts = updatedPosts;
+            this.postsUpdated.next([...this.posts]);
+        });
     }
 }
